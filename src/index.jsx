d798e74f2467d5b18527f9487aca0f8942c41237
Tomer Carolla <carollatomer@gmail.com>
2024-09-14 19:23:08 +0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import {createRoot} from 'react-dom/client'
-import {Provider} from 'react-redux'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-import {store} from './store/store'
-import {App} from './App.jsx'
+import {createRoot} from 'react-dom/client';
+import {Provider} from 'react-redux';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import {store} from './store/store';
+import {App} from './App.jsx';
 import {I18nextProvider} from "react-i18next";
 import {i18n} from "./utils/i18n.jsx";
-import './assets/styles/main.scss'
+import './assets/styles/main.scss';
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
@@ -18,4 +18,4 @@ createRoot(document.getElementById('root')).render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register()
+serviceWorkerRegistration.register();
