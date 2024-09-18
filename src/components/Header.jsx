c94ecc406f71652c13d721a35fe8022b7790ee7c
roylivnegit <--global>
2024-09-19 00:15:57 +0300

    first header commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,29 @@
 import { MenuOutlined, DownOutlined, QuestionCircleOutlined, BellOutlined, SearchOutlined } from '@ant-design/icons';
 
-const dropdowns = ["Workspaces", "Recent", "Starred", "Templates"]
+const dropdowns = ['Workspaces', 'Recent', 'Starred', 'Templates']
 
 export function Header() {
     return (
-        <div className="header-container">
+        <div className='header-container'>
             <div className='header-left-side'>
-                <div className="application-switcher-icon"><MenuOutlined /></div>
-                <div className="logo"><strong>Prello</strong></div>
-                {dropdowns.map((text, i) => <div key={i} className='drop-down-button'> {text} <DownOutlined /></div>)}
+                <div className='application-switcher-icon pointer'><MenuOutlined /></div>
+                <div className='logo pointer'><strong>Prello</strong></div>
+                <div className='drop-down-buttons-container pointer'>
+                    {dropdowns.map((text, i) => <div key={i} className='drop-down-button'> {text} <DownOutlined className='arrow-down-icon' /></div>)}
+                </div>
                 <button className='button-create'> Create </button>
             </div>
 
 
             <div className='header-right-side'>
                 <div className='input-container'>
-                    <SearchOutlined className='search-icon' />
+                    <SearchOutlined className='search-icon pointer' />
                     <input className='search-bar' placeholder='Search'>
                     </input>
                 </div>
-                <BellOutlined className='notifications' />
-                <QuestionCircleOutlined className='information' />
-                <div className='account-initials'> hi</div>
+                <BellOutlined className='notifications pointer' />
+                <QuestionCircleOutlined className='information pointer' />
+                <div className='account-initials pointer'> hi</div>
                 <div ></div>
             </div>
 
