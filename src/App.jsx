e06d0a01dbbf11b2b252364cd06742e663d4f61c
Yehonatan_Yeshayahu <73723174+JonathanYesh279@users.noreply.github.com>
2024-09-19 17:06:59 +0300

    Sidebar (#8)

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,32 @@ import {Routes, Route} from 'react-router'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import {HomePage} from './pages/HomePage'
+import { Sidebar } from './components/Sidebar.jsx';
 import {Header} from "./components/Header.jsx";
 import {Board} from "./components/board/Board.jsx";
 
 export function App() {
     return (
-        <div className='surface' style={{backgroundColor: 'rgb(75, 191, 107)'}}>
-            <Header />
+    <Router>
+        <div
+          className='surface'
+          style={{ backgroundColor: 'rgb(75, 191, 107)' }}
+        >
+          <Header />
 
-            <main>
-                <div className="container">
-                    <div className="nav" style={{backgroundColor: 'red'}}>nav here</div>
+        <main>
+            <div className='container'>
+              <Sidebar />
 
-                    <div className="content">
-                        <Router>
-                            <Routes>
-                                <Route path="/" element={<HomePage />}/>
-                                <Route path="/b/:id/:boardName" element={<Board />}/>
-                            </Routes>
-                        </Router>
-                    </div>
-                </div>
-            </main>
+              <div className='content'>
+                  <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='/b/:id/:boardName' element={<Board />} />
+                  </Routes>
+              </div>
+            </div>
+          </main>
         </div>
-    )
+    </Router>
+    );
 }
