06c8837bd1c27bdef604f514dd49d8dfb8052ce5
Jonathan_Yeshayahu <73723174+JonathanYesh279@users.noreply.github.com>
2024-09-21 14:52:39 +0300

    add Menu cmp to Sidebar buttons menu

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,34 +1,73 @@
 import {useState} from 'react';
-import {Link, NavLink} from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+
 import styled from 'styled-components';
 import { ProjectOutlined } from '@ant-design/icons'
+import { Select } from 'antd';
 
 
 import {Icon} from 'ui/icons/Icon';
-import {Button} from '@ui';
-import { DetailsDialog } from './DetailsDialog';
+import {Button, Menu} from '@ui';
+
+
 
 
 export function Sidebar() {
     const [expandedSidebar, setExpandedSidebar] = useState(true);
-    const [dialogDetails, setDialogDetails] = useState({
-        open: false,
-        position: '',
-        type: ''
-    });
+    const [openMenu, setOpenMenu] = useState(null);
 
-    function toggleDetailsDialog(position, type) {
-        setDialogDetails((prev) => {
-        if (prev.open && prev.position === position && prev.type === type) {
-          // If the same dialog is already open, close it
-          return { open: false, position: '', type: '' };
-        } else {
-          // Otherwise, open the new dialog
-          return { open: true, position, type };
-        }
-      });
+    function toggleMenu(menuType) {
+        setOpenMenu(openMenu === menuType ? null : menuType)
+  }
+  
+  function getMenuContent(type) {
+    switch (type) {
+      case 'workspace':
+        return (
+          <div className='filter-container'>
+            <StyledLabel>Filter</StyledLabel>
+            <StyledSelect
+              defaultValue='all'
+              style={{ width: '100%' }}
+              onChange={(value) => console.log(value)}
+              options={[
+                { value: 'all', label: 'All workspace views' },
+                { value: 'created', label: 'Created by me' },
+              ]}
+              className='custom-select'
+            />
+          </div>
+        );
+
+      case 'board':
+        return (
+          <div className='sort-container'>
+            <StyledLabel>Sort</StyledLabel>
+            <StyledSelect
+              defaultValue='alphabetically'
+              style={{ width: '100%' }}
+              onChange={(value) => console.log(value)}
+              options={[
+                { value: 'alphabetically', label: 'sort alphabetically' },
+                { value: 'created', label: 'sort by created date' },
+              ]}
+              className='custom-select'
+            />
+          </div>
+        );
+
+      case 'table':
+      case 'calendar':
+        return (
+          <Button scale='dynamic' size='md' className='close-button'>
+            <Icon name='trash' size='16px' color='var(--ds-text)' />
+            <span style={{ color: 'var(--ds-text)' }}>Remove View</span>
+          </Button>
+        );
+      default:
+        return null;
     }
-
+  }
     return (
       <>
         <nav className={`sidebar ${expandedSidebar ? 'expanded' : ''}`}>
@@ -82,15 +121,21 @@ export function Sidebar() {
               <div className='workspace-container'>
                 <span>Workspace views</span>
                 <div className='icons-container'>
-                  <Button
-                    size='sm'
-                    scale='ghost'
-                    className='details'
-                    onClick={(ev) => toggleDetailsDialog('top', 'workspace')}
+                  <Menu
+                    trigger={
+                      <Button
+                        size='sm'
+                        scale='ghost'
+                        className='details'
+                        onClick={() => toggleMenu('workspace')}
+                      >
+                        <Icon name='details' size='16px' />
+                      </Button>
+                    }
+                    title='Workspace Views'
                   >
-                    <Icon name='details' size='16px' />
-                  </Button>
-
+                    {getMenuContent('workspace')}
+                  </Menu>
                   <Button
                     size='sm'
                     scale='ghost'
@@ -110,14 +155,21 @@ export function Sidebar() {
                       <span>Tabel</span>
                     </div>
                     <div className='icons-container'>
-                      <Button
-                        size='md'
-                        scale='ghost'
-                        className='details'
-                        onClick={(ev) => toggleDetailsDialog('up', 'table')}
+                      <Menu
+                        trigger={
+                          <Button
+                            size='md'
+                            scale='ghost'
+                            className='details'
+                            onClick={() => toggleMenu('table')}
+                          >
+                            <Icon name='details' size='14px' />
+                          </Button>
+                        }
+                        title='Table'
                       >
-                        <Icon name='details' size='14px' />
-                      </Button>
+                        {getMenuContent('table')}
+                      </Menu>
                     </div>
                   </StyledLink>
                 </li>
@@ -125,18 +177,25 @@ export function Sidebar() {
                 <li className='list-item'>
                   <StyledLink to='/'>
                     <div className='icons-container'>
-                      <Icon name='calendar' size='16px' />
+                      <Icon name='table' size='16px' />
                       <span>Calendar</span>
                     </div>
                     <div className='icons-container'>
-                      <Button
-                        size='md'
-                        scale='ghost'
-                        className='details'
-                        onClick={(ev) => toggleDetailsDialog('middle', 'calendar')}
+                      <Menu
+                        trigger={
+                          <Button
+                            size='md'
+                            scale='ghost'
+                            className='details'
+                            onClick={() => toggleMenu('calendar')}
+                          >
+                            <Icon name='details' size='14px' />
+                          </Button>
+                        }
+                        title='Table'
                       >
-                        <Icon name='details' size='14px' />
-                      </Button>
+                        {getMenuContent('calendar')}
+                      </Menu>
                     </div>
                   </StyledLink>
                 </li>
@@ -145,14 +204,21 @@ export function Sidebar() {
               <div className='workspace-container'>
                 <span>Your Boards</span>
                 <div className='icons-container'>
-                  <Button
-                    size='sm'
-                    scale='ghost'
-                    className='details'
-                    onClick={() => toggleDetailsDialog('down', 'board')}
+                  <Menu
+                    trigger={
+                      <Button
+                        size='sm'
+                        scale='ghost'
+                        className='details'
+                        onClick={() => toggleMenu('board')}
+                      >
+                        <Icon name='details' size='16px' />
+                      </Button>
+                    }
+                    title='Your boards'
                   >
-                    <Icon name='details' size='16px' />
-                  </Button>
+                    {getMenuContent('board')}
+                  </Menu>
                   <Button
                     size='sm'
                     scale='ghost'
@@ -165,45 +231,45 @@ export function Sidebar() {
               </div>
 
               <List>
-                <li className='list-item'>
-                  <StyledLink to='/'>
-                    <div>
-                      <span style={{ marginRight: '8px' }}>
-                        <ProjectOutlined />
-                      </span>
-                      <span>Yonatan's Board</span>
-                    </div>
-                    <div className='icons-container'>
-                      <Button
-                        size='md'
-                        scale='ghost'
-                        className='details'
-                        onClick={() => toggleDetailsDialog('bottom', 'board')}
-                      >
-                        <Icon name='details' size='16px' />
-                      </Button>
-                      <Button
-                        size='md'
-                        scale='ghost'
-                        className='star'
-                        onClick={() => console.log('Star this board')}
-                      >
-                        <Icon name='starEmpty' size='16px' />
-                      </Button>
-                    </div>
-                  </StyledLink>
-                </li>
+                  <li className='list-item'>
+                    <StyledLink to='/'>
+                      <div>
+                        <span style={{ marginRight: '8px' }}>
+                          <ProjectOutlined />
+                        </span>
+                        <span>Yonatan's Board</span>
+                      </div>
+                      <div className='icons-container'>
+                        <Menu
+                          trigger={
+                            <Button
+                              size='md'
+                              scale='ghost'
+                              className='details'
+                              onClick={() => toggleMenu('board')}
+                            >
+                              <Icon name='details' size='16px' />
+                            </Button>
+                          }
+                          title='Board Options'
+                        >
+                          {getMenuContent('board')}
+                        </Menu>
+                        <Button
+                          size='md'
+                          scale='ghost'
+                          className='star'
+                          onClick={() => console.log('Star this board')}
+                        >
+                          <Icon name='starEmpty' size='16px' />
+                        </Button>
+                      </div>
+                    </StyledLink>
+                  </li>
               </List>
             </>
           )}
         </nav>
-        {dialogDetails.open && (
-            <DetailsDialog
-                onToggle={toggleDetailsDialog}
-                position={dialogDetails.position} 
-                type={dialogDetails.type}    
-                />
-        )}
       </>
     );
 }
@@ -257,4 +323,35 @@ const StyledLink = styled(Link)`
         background-color: var(--ds-background-neutral-hovered);
         text-decoration: none;
     }
+`;
+
+const StyledSelect = styled(Select)`
+  .ant-select-selector {
+    background-color: var(--surface) !important;
+    color: var(--ds-text) !important;
+    border: 1px solid transparent !important;
+  }
+
+  &:hover .ant-select-selector {
+    border-color: var(--ds-border-input-hovered) !important;
+  }
+
+  .ant-select-arrow {
+    color: var(--ds-text) !important;
+  }
+
+  &.ant-select-focused .ant-select-selector {
+    background-color: #22272b !important;
+    border-color: #579dff !important;
+    color: var(--ds-text) !important;
+  }
+
+  &.ant-select-focused .ant-select-arrow {
+    color: var(--ds-text) !important;
+  }
+`;
+
+const StyledLabel = styled.label`
+  font-size: 12px;
+  font-weight: 600;
 `;
