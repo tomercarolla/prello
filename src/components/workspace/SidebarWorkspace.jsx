a388e7e63e578459bee82abe1297795c32281d80
Jonathan_Yeshayahu <73723174+JonathanYesh279@users.noreply.github.com>
2024-09-26 21:18:33 +0300

    styling sidebar of workspace page

--- src/components/workspace/SidebarWorkspace.jsx
+++ src/components/workspace/SidebarWorkspace.jsx
@@ -1,7 +1,44 @@
+import { useState } from 'react';
+import { Divider } from 'components/sidebar/StyledElements'
+import { Link } from "react-router-dom";
+
 export function SidebarWorkspace() { 
+ const [isMenuOpen, setIsMenuOpen] = useState(false);
+
  return (
-  <>
-   <h1>Navbar</h1>
-  </>
- )
+   <>
+     <nav className='sidebar-workspace'>
+       <div className='top-sidebar-container'>
+         <ul className='top-sidebar'>
+           <li className='top-list-item'>
+             <Link to='/w'>
+               Boards
+             </Link>
+           </li>
+           <li className='top-list-item'>
+             <Link to='/templates'>
+               Templates
+             </Link>
+           </li>
+           <li className='top-list-item'>
+             <Link to='/'>
+               Home
+             </Link>
+           </li>
+         </ul>
+    </div>
+    
+    <Divider />
+
+         <ul className='bottom-sidebar'>
+           <div className='workspaces'>
+             <span>Workspaces</span>
+           </div>
+
+           <li className='dropdown-menu-header'><div></div>Tomer test</li>
+             <ul className='dropdown-menu'></ul>
+         </ul>
+     </nav>
+   </>
+ );
 }
