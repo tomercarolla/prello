40d366120423a0eb9b5e97ee1225c78bf83015f0
Jonathan_Yeshayahu <73723174+JonathanYesh279@users.noreply.github.com>
2024-12-03 18:45:40 +0200

    continue working on login and signup pages styling

--- src/pages/LoginSignupPage/LoginPage.jsx
+++ src/pages/LoginSignupPage/LoginPage.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { login } from "store/user/user.actions.js"
+
+import leftImage from './assets/trello-signup-left-image.svg';
+import rightImage from './assets/trello-signup-right-image.svg';
 
 export function LoginPage() {
   const navigate = useNavigate()
@@ -20,30 +23,44 @@ export function LoginPage() {
   return (
     <div className="login-page">
       <form onSubmit={handleSubmit}>
+
+        <h2>Login</h2>
+        <label htmlFor="username">Username</label>
         <input
           type="text"
           name="username"
           value={credentials.username}
-          onChange={ev => setCredentials(prev => ({
-            ...prev,
-            username: ev.target.value
-          }))}
+          onChange={(ev) =>
+            setCredentials((prev) => ({
+              ...prev,
+              username: ev.target.value,
+            }))
+          }
           placeholder="Username"
         />
-
+        <label htmlFor="password">Password</label>
         <input
           type="password"
           name="password"
           value={credentials.password}
-          onChange={ev => setCredentials(prev => ({
-            ...prev,
-            password: ev.target.value
-          }))}
+          onChange={(ev) =>
+            setCredentials((prev) => ({
+              ...prev,
+              password: ev.target.value,
+            }))
+          }
           placeholder="Password"
         />
         <button>Login</button>
+        <div className="signup-link">
+          Don't have an account? <Link to="/signup">Sign up</Link>
+        </div>
       </form>
 
+      <div className="images-container">
+        <img src={leftImage} alt="left-image" />
+        <img src={rightImage} alt="right-image" />
+      </div>
     </div>
   )
 }
